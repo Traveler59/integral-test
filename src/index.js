141944ce26f0bbf197c85080079070a213ebddbf
Traveler59 <dmitry_alekseevich@mail.ru>
2019-08-16 08:37:30 +0300

    Redux Persist добавлен

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './containers/App';
 import configureStore from './store/configureStore';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,26 @@
 // @flow
 import { createStore } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
 import rootReducer from '../reducers';
 
 import type { State } from '../types/types';
 
+
+const persistedReducer = persistReducer({
+  key: 'root',
+  storage,
+}, rootReducer);
+
 const configureStore = (initialState: State) => {
-  const store = createStore<*, *, *>(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-  return store;
+  const store = createStore<*, *, *>(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  const persistor = persistStore(store);
+  return { store, persistor };
 };
 
 export default configureStore;
